eaaee75ccc6873eabd03a4278aa6ed1d72188bcf
Nikita Korolkov <nikitakorolkov@Nikitas-MacBook-Pro.local>
2023-02-10 13:16:00 -0500

    configured vercel.json

--- index.js
+++ index.js
@@ -6,14 +6,6 @@ mongoose
   .connect("mongodb+srv://nk:admin@cluster0.nc6ci.mongodb.net/JeansShop?retryWrites=true&w=majority", { useNewUrlParser: true })
   .then(() => {
     const app = express();
-    app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-      );
-      next();
-    });
     app.use(express.json());
     app.use("/api", routes);
 
